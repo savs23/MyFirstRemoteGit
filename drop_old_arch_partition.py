db4b41f7402fc295d7b90248e95f429735e58e50
savs23 <a.shtepa23@gmail.com>
2016-01-24 11:45:49 +0200

    #bla bla bla at Dev Remote Edit bla bla

--- drop_old_arch_partition.py
+++ drop_old_arch_partition.py
@@ -13,7 +13,7 @@ import sys
 import os
 import optparse
 #Switched to branch 'MyDev'
-#bla bla bla at Dev
+#bla bla bla at Dev Remote Edit bla bla
 
 #Add blank row ALMOST on the top on the local comp. I try correct in the same row on LOCAL COMP
 parser = optparse.OptionParser()
